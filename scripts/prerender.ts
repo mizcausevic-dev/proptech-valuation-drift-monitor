f7a99c54eac3a547692b8ec094c79de05f49bd4c
fknmiz <150495873+fknmiz@users.noreply.github.com>
2026-06-08 16:33:53 -0400

    Enrich public evidence surface

--- scripts/prerender.ts
+++ scripts/prerender.ts
@@ -5,3 +5,5 @@ import type { ValuationInput } from "../src/index.js";
 const input = JSON.parse(fs.readFileSync("fixtures/valuation-drift.json", "utf8")) as ValuationInput;
 fs.mkdirSync("site", { recursive: true });
 fs.writeFileSync("site/index.html", renderApp(input));
+
+fs.writeFileSync("site/sitemap.xml", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n  <url><loc>https://mizcausevic-dev.github.io/proptech-valuation-drift-monitor/</loc></url>\n</urlset>\n");
